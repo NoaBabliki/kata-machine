6b6553db0999f6b9b1932d072fa0d2b413d6564f
Noa Babliki <noababliki@Noas-MacBook-Pro.local>
2024-04-14 17:16:07 +0300

    binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,18 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let low = 0;
+    let high = haystack.length;
+    while (high > low) {
+        const pivot = Math.floor(low + (high - low) / 2);
+        const currrentValue = haystack[pivot]
+        if (currrentValue === needle) {
+            return true;
+        }
+        if (currrentValue < needle) {
+            low = pivot + 1;
+        }
+        if (currrentValue > needle) {
+            high = pivot;
+        }
+    }
+    return false;
 }
