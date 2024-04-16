d54a96c414b0d59b8c8e52de19f7592f731cc2f6
Noa Babliki <noababliki@Noas-MacBook-Pro.local>
2024-04-16 11:56:08 +0300

    bubbleSort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,25 @@
 export default function bubble_sort(arr: number[]): void {
-
+    // my solution:
+    for (var i = arr.length; i > 0; --i) {
+        for (var j = 0; j < i; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                const temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
 }
+
+// school solution:
+// export default function bubble_sort(arr: number[]): void {
+//     for (let i = 0; i > arr.length; ++i) {
+//         for (let j = 0; j < arr.length - 1 - i; ++j) {
+//             if (arr[j] > arr[j + 1]) {
+//                 const tmp = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j + 1] = tmp;
+//             }
+//         }
+//     }
+// }
